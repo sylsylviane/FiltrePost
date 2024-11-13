1d5a7dd8fac2e24070580f2c0e7949119525c051
sylsylviane <syl.sylviane1@gmail.com>
2024-11-13 14:40:37 -0500

    Charger fichier js et css

---
+++ filtrepost.php
@@ -0,0 +1,30 @@
+<?php
+/*
+plugin name: Filtre post
+author: Sylviane Paré
+description: Une extension qui permettra de filtrer nos articles
+author uri: https://www.gftnth00.mywhc.ca/31w09
+*/
+
+function charger_scripts_css(){
+
+    $version_css = filemtime(plugin_dir_path(__FILE__). "/style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/filtrepost.js");
+
+    wp_enqueue_style(
+        "filtrepost",        
+        plugin_dir_url(__FILE__) . "/style.css",
+        array(),
+        $version_css
+    ) ;  
+
+    wp_enqueue_script( //ici, pas de s à la fin de wp_enqueue_script
+        "filtrepost",       
+        plugin_dir_url(__FILE__) . "/js/filtrepost.js",
+        array(),
+        $version_js,
+        true
+    )  ; 
+}
+
+add_action("wp_enqueue_scripts", "charger_scripts_css");
